11dec2b6c605eb75222e9801d513dd35a68cf4a5
renming-lj <2982488597@qq.com>
2022-10-07 09:50:27 +0800

    server

--- server/app.js
+++ server/app.js
@@ -35,5 +35,5 @@ app.get('/data', function(req, res) {
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Example app listening at http://127.0.0.1:3000', host, port);
+    console.log('Example app listening at http://127.0.0.1', host, port);
 })
